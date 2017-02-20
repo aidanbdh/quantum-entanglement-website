7da9a4b6b7182ffbe2c288cccf25972c3ee55db9
Aidan Harvey <aidanbdh1998@gmail.com>
2017-02-19 23:00:04 -0800

    Website done except for socket connection issue and deployment #3

--- public/populations.js
+++ public/populations.js
@@ -1,11 +1,41 @@
 const React = require('react')
 const { connect } = require('react-redux')
-const actions = require('./actions')
+const { Population } = require('./actions')
 
-const populations = () => {}
+const populations = ({ server1, server2, server3 }) =>
+<div>
+{
+  Population()
+}
+  <div className="server">
+    <h2>Server 1</h2>
+    <div className="content">
+      <p>Status: { server1.status }</p>
+      <p>Connections: { server1.count }</p>
+    </div>
+  </div>
+  <div className="server">
+    <h2>Server 2</h2>
+    <div className="content">
+      <p>Status: { server2.status }</p>
+      <p>Connections: { server2.count }</p>
+    </div>
+  </div>
+  <div className="server">
+    <h2>Server 3</h2>
+    <div className="content">
+      <p>Status: { server3.status }</p>
+      <p>Connections: { server3.count }</p>
+    </div>
+  </div>
+</div>
 
-const mapDispatchToProps = dispatch => {
-  return dispatch
+const mapStateToProps = ({ serverPopulation }) => {
+  return {
+    server1: serverPopulation.server1,
+    server2: serverPopulation.server2,
+    server3: serverPopulation.server3
+  }
 }
 
-module.exports = connect(mapDispatchToProps)(populations)
+module.exports = connect(mapStateToProps)(populations)
